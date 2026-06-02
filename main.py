f0c787f7dde9034e5c91b9ade339f739dc1dfe52
Tarik <tarikkaplan2002@gmail.com>
2026-06-02 11:25:13 -0400

    Betting System Added

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ WIDTH, HEIGHT = 500, 500
 
 COLORS = ['red', 'green', 'blue', 'orange', 'yellow', 'black', 'purple', 'pink', 'brown', 'cyan']
 
+balance = 50
+
 def get_number_of_racers():
     racers = 0
     while True:
@@ -38,7 +40,8 @@ def race(colors):
                 return colors[turtles.index(racer)]
 
 
-
+def controlled_turtle():
+    pass
 
 # 'colors' define the turtle individuals, basically number of colors define howmany turtles will be racing
 def create_turtles(colors):
@@ -56,6 +59,7 @@ def create_turtles(colors):
 
     return turtles
 
+
 #Initialize the Turtle Race Screen
 def init_turtle():
     screen = turtle.Screen()
@@ -67,8 +71,65 @@ print(f"{racers} number of turtles will be racing ...")
 
 init_turtle()
 
+def get_bet():
+    global balance
+
+    print(f"Your balance is {balance}$")
+    if balance <= 0:
+        print("Sorry, your balance is not sufficient. Can not bet ...")
+    else:
+        while True:
+            betAmount = input("Enter the amount you want to bet: ")
+            if betAmount.isdigit():
+                betAmount = float(betAmount)
+            else:
+                print("Enter a sufficient number...")
+                continue
+            
+            if betAmount <= balance:
+                balance = balance - betAmount
+                return betAmount
+            else:
+                print("Insufficient balance...")
+                continue
+
+def bet_calculation(betAmount, winner, users_turtle):
+    
+    global balance
+
+    if users_turtle == winner:
+        print("You Won !!!")
+        balance = (betAmount * 2) + balance
+        print(f"Your balance went up to {balance}$")
+    else:
+        print("Unfortunately your turtle lost ...")
+        
+    
+
+
+
+
 random.shuffle(COLORS)
 colors = COLORS[:racers] # slice up to 'racers' amount of racers    Ex: [1,2,3,4] [:2] => [1,2]
 
+formatted_colors = " - ".join(colors)
+
+while True:
+    users_turtle = input(f"{formatted_colors} colored turtles will be racing, bet on one: ").lower()
+
+    if users_turtle not in colors:
+        print("Invalid turtle...")
+        continue
+    else:
+        index = colors.index(users_turtle)
+        break
+
+
+bet_amount = get_bet()
+
+
 winner = race(colors)
-print(f"Winner turtle is the {winner} turtle !!!")
+
+bet_calculation(bet_amount, winner, users_turtle)
+
+print(f"Winner turtle is the {winner} turtle")
